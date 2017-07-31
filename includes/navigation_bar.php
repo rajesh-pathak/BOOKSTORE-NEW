3cfe90b0b0ef0973b30c0fa5e37044bd1f6731d5
rajesh-pathak <myraj007@gmail.com>
2017-07-31 16:59:25 +0530

    Add files via upload

---
+++ includes/navigation_bar.php
@@ -0,0 +1,66 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">BOOKSTORE</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li ><a href="home.php">Home <span class="sr-only"></span></a></li>
+        <li><a href="overview.php">Overview</a></li>
+        
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Awarness Program <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="elec_safety.php">Electrical Safety</a></li>
+             <li role="separator" class="divider"></li>
+            <li><a href="#"></a></li>
+            <li style="text-align:center">More Programs <br /> Coming Soon</li>
+          </ul>
+        </li>
+        
+
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Available Courses <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li>Coming Soon</a></li>
+
+          </ul>
+        </li>
+        
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Upcoming Courses <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Fire & Safety</a></li>
+            <li><a href="#">Enviornmental Studies</a></li>
+          </ul> 
+        </li>  
+
+
+        <li> <a href="contact.php"> Contact-Info</a></li>
+      </ul>
+      
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">External Links <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="http://nss.cusat.ac.in">NSS home</a></li>
+            <li><a href="http://nss.cusat.ac.in/goorganic/home.php">Go-Organic</a></li>
+            <li><a href="swayam.php">Swayam </a></li>
+          </ul>
+        </li>
+        
+
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
